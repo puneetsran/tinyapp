692dc6fd7ed685c51cd6be4e3a82dfc8c9c1af5e
Puneet Sran <puneet_sran@icloud.com>
2019-10-01 02:32:39 +0000

    implemented redirection after submission and redirection of short URLs

--- express_server.js
+++ express_server.js
@@ -46,9 +46,15 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let string = generateRandomString();
+  urlDatabase[string] = req.body.longURL;
+  res.redirect(`/urls/${string}`);
 });
 
 function generateRandomString() {
